03c1f08ef3e0b751cc72f0e9d108891c31401bbd
assemmomo <106935023+assemmomo@users.noreply.github.com>
2025-09-09 14:40:01 +0300

    Update view.js

--- src/js/views/view.js
+++ src/js/views/view.js
@@ -6,6 +6,40 @@ export default class View{
         this._clear();
         this._parentElement.insertAdjacentHTML('afterbegin', markup);
     }
+    update(data){
+        if (!data || (Array.isArray(data) && data.length === 0)) return;
+        this._data = data;
+        const newMarkup = this._generateMarkup();
+
+        const newDOM = document.createRange().createContextualFragment(newMarkup);
+        const newElements = Array.from(newDOM.querySelectorAll('*'));
+        const curElements = Array.from(this._parentElement.querySelectorAll('*'));
+
+       newElements.forEach((newEl, i) => {
+            const curEl = curElements[i];
+            if (curEl) {
+                if (!newEl.isEqualNode(curEl) && newEl.firstChild && newEl.firstChild.nodeValue !== null && newEl.firstChild.nodeValue.trim() !== ''){
+                    curEl.textContent = newEl.textContent;
+                }
+                if (!newEl.isEqualNode(curEl)){
+                    Array.from(newEl.attributes).forEach(attr => {
+                        curEl.setAttribute(attr.name, attr.value);
+                    });
+                }
+            }
+        });
+
+
+
+        // newElements.forEach((newEl, i) => {
+        //     const curEl = curElements[i];
+        //     // Check if firstChild exists before accessing nodeValue
+        //     if (!newEl.isEqualNode(curEl) && newEl.firstChild && newEl.firstChild.nodeValue.trim() !== ''){
+        //         curEl.textContent = newEl.textContent;
+        //     }
+        // });
+
+    }
 
     _clear(){
         this._parentElement.innerHTML = '';
