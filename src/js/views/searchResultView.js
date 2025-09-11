42a416a0b65edaf15fb91522f7e429319dfa1d7c
assemmomo <106935023+assemmomo@users.noreply.github.com>
2025-09-11 07:08:35 +0300

    Update searchResultView.js

--- src/js/views/searchResultView.js
+++ src/js/views/searchResultView.js
@@ -6,9 +6,11 @@ class searchResultView extends View {
         return this._data.map(this._generateMarkupPreview).join('');
     }
     _generateMarkupPreview(result) {
+        const id = window.location.hash.slice(2);
+        showSideBar()
         return `
         <li>
-            <a href="#&${result.id}">
+            <a href="#&${result.id}" class="${id === result.id ? 'link_active' : ''}">
                 <img src="${result.image}" alt="${result.title}">
                 <div>
                     <h4>${result.title}</h4>
@@ -18,5 +20,12 @@ class searchResultView extends View {
         </li>
         `
     }
+}
+function showSideBar() {
+    const sideBar = document.querySelector('.sideBar');
+    const openSearchResult = document.querySelector('.openSearchResult');
+    openSearchResult.querySelector('i').classList.remove('fa-bars');
+    openSearchResult.querySelector('i').classList.add('fa-xmark');
+    sideBar.classList.add('showSideBar');
 }
 export default new searchResultView();
